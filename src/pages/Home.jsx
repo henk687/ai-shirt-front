de86a0248b1f7ea5786652743ae6777efe697ad1
henk687 <henkleenheer@upcmail.nl>
2023-06-22 00:12:27 +0200

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
           <motion.div className="home-content" {...headContainerAnimation}>
             <motion.div {...headTextAnimation}>
               <h1 className="head-text">
-                DESIGN <br className="xl:block hidden" /> SHIRT.
+                ONTWERP <br className="xl:block hidden" /> JE SHIRT.
               </h1>
             </motion.div>
             <motion.div
